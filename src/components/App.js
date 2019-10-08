238bd6f59172696a4f5f89f47669c68b2ce6142e
Intellisys <intellisys@Intellisyss-Mac-mini.local>
2019-10-08 16:28:22 -0400

    changes classes to hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import ScoreBoard from './ScoreBoard';
 import Players from './Players';
 import AddPlayer from './AddPlayer';
@@ -8,59 +8,42 @@ import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-do
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
 
-class App extends Component {
-  componentDidMount() {
-    this.props.dispatch(handleInitialData())
-  }
+function App(props) {
+  useEffect(() => {
+    props.dispatch(handleInitialData())
+  })
 
-  render() {
+  return (
+    <div className="container" >
+      <Router>
+        <div>
+          <nav className='nav'>
+            <ul>
+              <li>
+                <NavLink to="/" exact activeClassName="active" >Score Board</NavLink>
+              </li>
+              <li>
+                <NavLink to="/players" activeClassName="active">Players</NavLink>
+              </li>
+              <li>
+                <NavLink to="/add-player" activeClassName="active">Add Player</NavLink>
+              </li>
+            </ul>
+          </nav>
 
-    return (
-      <div className="container" >
-        <Router>
-          <div>
-            <nav className='nav'>
-              <ul>
-                <li>
-                  <NavLink to="/" exact activeClassName="active" >Score Board</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/players" activeClassName="active">Players</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/add-player" activeClassName="active">Add Player</NavLink>
-                </li>
-              </ul>
-            </nav>
-
-            <Switch>
-              <Route path="/" exact>
-                <ScoreBoard />
-                <Search/>
-              </Route>
-              <Route path="/players" component={
-                (props) => <Players
-                  {...props}
-                />
-              } />
-              <Route path="/add-player" component={
-                (props) => <AddPlayer
-                  {...props}
-                />
-              } />
-              <Route path="/edit-player/:id" component={
-                (props) => <AddPlayer
-                  {...props}
-                  onEditPlayer={(this.handleEditPlayer)}
-                />
-              } />
-
-            </Switch>
-          </div>
-        </Router>
-      </div>
-    );
-  }
+          <Switch>
+            <Route path="/" exact>
+              <ScoreBoard />
+              <Search />
+            </Route>
+            <Route path="/players" component={Players} />
+            <Route path="/add-player" component={AddPlayer} />
+            <Route path="/edit-player/:id" component={AddPlayer} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default connect()(App);
